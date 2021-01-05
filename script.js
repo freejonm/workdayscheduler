7f54a265cb9f125dae4ba6e56a722fb832325cfe
Jon Free <freejonm@gmail.com>
2021-01-05 12:03:51 -0500

    adding a timeblock with background color

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-console.log(moment());
-
+// set a variable for the current date/time
 var now = moment();
 
+// print current date/time in browser
 $("#currentDay").text("Current date/time: " + now._d);
+
+// timeblocks change color if it is currently later
+
+
+$("#9").css("background-color", "green");
+
+// edit timeblocks with tasks
+
+// 
