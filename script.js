e41572adda676f6ce9a70b422552c4d82c5a9b74
Jon Free <freejonm@gmail.com>
2021-01-05 13:55:10 -0500

    changed = in if statement to => so color change is accurate

--- script.js
+++ script.js
@@ -1,60 +1,61 @@
 // set a variable for the current date/time
 var now = moment();
-console.log(now.hour());
+console.log(now);
 // print current date/time in browser
 $("#currentDay").text("Current date/time: " + now._d);
 
+console.log(now.locale.isPM)
 // timeblocks change color if it is currently later
-if(now.hour() > 10){
+if(now.hour() >= 10){
     $("#9").css("background-color", "red");
 }
 else {
     $("#9").css("background-color", "green");
 }
 
-if(now.hour() > 11){
+if(now.hour() >= 11){
     $("#10").css("background-color", "red");
 }
 else {
     $("#10").css("background-color", "green");
 }
 
-if(now.hour() > 12){
+if(now.hour() >= 12){
     $("#11").css("background-color", "red");
 }
 else {
     $("#11").css("background-color", "green");
 }
 
-if(now.hour() > 13){
+if(now.hour() >= 13){
     $("#12").css("background-color", "red");
 }
 else {
     $("#12").css("background-color", "green");
 }
 
-if(now.hour() > 14){
+if(now.hour() >= 14){
     $("#1").css("background-color", "red");
 }
 else {
     $("#1").css("background-color", "green");
 }
 
-if(now.hour() > 15){
+if(now.hour() >= 15){
     $("#2").css("background-color", "red");
 }
 else {
     $("#2").css("background-color", "green");
 }
 
-if(now.hour() > 16){
+if(now.hour() >= 16){
     $("#3").css("background-color", "red");
 }
 else {
     $("#3").css("background-color", "green");
 }
 
-if(now.hour() > 17){
+if(now.hour() >= 17){
     $("#4").css("background-color", "red");
 }
 else {
