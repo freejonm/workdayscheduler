f4049ea3dfa69b2cd45db499ab0170b4e0b7aa25
Jon Free <freejonm@gmail.com>
2021-01-05 15:23:11 -0500

    adding color change for current hour

--- script.js
+++ script.js
@@ -4,63 +4,78 @@ console.log(now);
 // print current date/time in browser
 $("#currentDay").text("Current date/time: " + now._d);
 
-console.log(now.locale.isPM)
+
 // timeblocks change color if it is currently later
-if(now.hour() >= 10){
-    $("#9").css("background-color", "red");
-}
-else {
-    $("#9").css("background-color", "green");
-}
 
-if(now.hour() >= 11){
-    $("#10").css("background-color", "red");
+// present
+if(now.hour() < 15){
+    $("3").css("background-color", "green");
 }
-else {
-    $("#10").css("background-color", "green");
-}
-
-if(now.hour() >= 12){
-    $("#11").css("background-color", "red");
-}
-else {
-    $("#11").css("background-color", "green");
-}
-
-if(now.hour() >= 13){
-    $("#12").css("background-color", "red");
-}
-else {
-    $("#12").css("background-color", "green");
-}
-
-if(now.hour() >= 14){
-    $("#1").css("background-color", "red");
-}
-else {
-    $("#1").css("background-color", "green");
-}
-
-if(now.hour() >= 15){
-    $("#2").css("background-color", "red");
-}
-else {
-    $("#2").css("background-color", "green");
-}
-
-if(now.hour() >= 16){
+//past
+else if (now.hour() > 15){
     $("#3").css("background-color", "red");
 }
+//future
 else {
-    $("#3").css("background-color", "green");
+    $("#3").css("background-color", "blue");
 }
 
-if(now.hour() >= 17){
-    $("#4").css("background-color", "red");
-}
-else {
-    $("#4").css("background-color", "green");
-}
-// edit timeblocks with tasks
 
-// 
+// if(now.hour() = 10){
+//     $("#9").css("background-color", "blue");
+// }
+// else {
+//     $("#9").css("background-color", "green");
+// }
+
+// if(now.hour() >= 11){
+//     $("#10").css("background-color", "red");
+// }
+// else {
+//     $("#10").css("background-color", "green");
+// }
+
+// if(now.hour() >= 12){
+//     $("#11").css("background-color", "red");
+// }
+// else {
+//     $("#11").css("background-color", "green");
+// }
+
+// if(now.hour() >= 13){
+//     $("#12").css("background-color", "red");
+// }
+// else {
+//     $("#12").css("background-color", "green");
+// }
+
+// if(now.hour() >= 14){
+//     $("#1").css("background-color", "red");
+// }
+// else {
+//     $("#1").css("background-color", "green");
+// }
+
+// if(now.hour() >= 15){
+//     $("#2").css("background-color", "red");
+// }
+// else {
+//     $("#2").css("background-color", "green");
+// }
+
+// if(now.hour() >= 16){
+//     $("#3").css("background-color", "red");
+// }
+// else {
+//     $("#3").css("background-color", "green");
+// }
+
+// if(now.hour() >= 17){
+//     $("#4").css("background-color", "red");
+// }
+// else {
+//     $("#4").css("background-color", "green");
+// }
+// edit timeblocks with events
+
+// save events in local storage
